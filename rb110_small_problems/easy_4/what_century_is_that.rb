0d27f861386ae6e33fe10f0f56c17a4be9929ef9
Eric Felner <felnerer@outlook.com>
2024-06-16 11:22:32 -0400

    more PEDAC with solutions added

--- rb110_small_problems/easy_4/what_century_is_that.rb
+++ rb110_small_problems/easy_4/what_century_is_that.rb
@@ -25,3 +25,37 @@ Data:
 - if end 4, 5, 6, 7, 8, 9, 0 add th
 
 =end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+
+=begin
+Given Solution:
+
+def century(year)
+  century = year / 100 + 1
+  century -= if year % 100 == 0
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+    when 1 then 'st'
+    when 2 then 'nd'
+    when 3 then 'rd'
+    else 'th'
+  end
+end
+
+=end
