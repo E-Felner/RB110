383910447f8ac3297ba7ad69fb3ef4ea195a0527
Eric Felner <felnerer@outlook.com>
2024-06-16 14:14:33 -0400

    updated easy 5, ascii

---
+++ rb110_small_problems/easy_5/ascii_string_value.rb
@@ -0,0 +1,27 @@
+# ascii string value
+
+=begin
+P:
+Write a method that takes a string as an argument and returns the 
+sum of the characters ascii values from the string. 
+
+- may use String#ord
+
+D:
+input: string
+- iterate through string getting the ascii values
+- add the values together
+- return sum of ascii values
+output: integer(sum of ascii values)
+
+
+=end
+
+def ascii_value
+  sum_of_values = 0
+  
+
+ascii_value('Four score') == 984
+ascii_value('Launch School') == 1251
+ascii_value('a') == 97
+ascii_value('') == 0
