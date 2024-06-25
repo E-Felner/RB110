bbc9107ebe72c04e5847c29e0fb4c5ae9cf3c966
Eric Felner <felnerer@outlook.com>
2024-06-25 13:08:33 -0400

    started easy 5, after midnight part 1

--- rb110_small_problems/easy_5/after_midnight_part1.rb
+++ rb110_small_problems/easy_5/after_midnight_part1.rb
@@ -1,6 +1,19 @@
 # after midnight part 1
 
+
+time_of_day(0) == "00:00"
+time_of_day(-3) == "23:57"
+time_of_day(35) == "00:35"
+time_of_day(-1437) == "00:03"
+time_of_day(3000) == "02:00"
+time_of_day(800) == "13:20"
+time_of_day(-4231) == "01:29"
+
 =begin
 
 Given Solution:
+
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
