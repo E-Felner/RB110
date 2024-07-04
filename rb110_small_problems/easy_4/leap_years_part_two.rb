7b35b5933ce2743d9f3ffa9f6e4cf716c4aa906d
Eric Felner <felnerer@outlook.com>
2024-07-04 09:51:10 -0400

    added tests to leap year2 easy 4

--- rb110_small_problems/easy_4/leap_years_part_two.rb
+++ rb110_small_problems/easy_4/leap_years_part_two.rb
@@ -8,7 +8,21 @@ P: take last method from part 1 and if the year is before 1752,
 D: 
 add the if statement to account for years before 1752
 
+=end
 
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == true
+p leap_year?(1) == false
+p leap_year?(100) == true
+p leap_year?(400) == true
 
 =begin
 
