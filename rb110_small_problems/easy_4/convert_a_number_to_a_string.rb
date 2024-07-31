9db9a24bf1250b4e631484289e3c8e749b6f36d3
Eric Felner <felnerer@outlook.com>
2024-07-31 14:01:30 -0400

    added to signed number problems

--- rb110_small_problems/easy_4/convert_a_number_to_a_string.rb
+++ rb110_small_problems/easy_4/convert_a_number_to_a_string.rb
@@ -29,3 +29,31 @@ def integer_to_string(number)
 end
 
 =end
+
+# convert a number to a string
+
+=begin
+write a method that takes a positive integer or zero and converts it to a string.
+
+- set a string array of numbers
+- use the remainder of dividing by 10 as index for string array
+- continue through the remaining integers
+- return the result joined to a string
+
+=end
+
+STRING_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(number)
+  array_results = []
+  loop do
+    number, remainder = number.divmod(10)
+    array_results.unshift(STRING_ARRAY[remainder])
+    break if number == 0
+  end
+  array_results.join
+end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
--- rb110_small_problems/easy_4/convert_a_signed_number_to_string.rb
+++ rb110_small_problems/easy_4/convert_a_signed_number_to_string.rb
@@ -21,3 +21,35 @@ def signed_integer_to_string(number)
 end
 
 =end
+
+# convert a signed number to a string
+
+=begin
+write a method that takes an integer, and converts it to string.
+
+- compare number to zero
+- when - is first then - string_to_integer
+- when + is first then + string_to_integer
+- when   is first then string_to_integer
+
+=end
+
+STRING_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(number)
+  array_results = []
+  loop do
+    number, remainder = number.divmod(10)
+    array_results.unshift(STRING_ARRAY[remainder])
+    break if number == 0
+  end
+  array_results.join
+end
+
+def signed_integer_to_string(number)
+  case number <=> 0
+  when -1 then "-#{integer_to_string(number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else         integer_to_string(number)
+  end
+end
